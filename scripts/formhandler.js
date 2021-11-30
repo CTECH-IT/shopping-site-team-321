d88578778698c6f47936cf5912d1cf817fa4bc8c
Elizabeth Ladwig <7528001@isd535.org>
2021-11-30 08:58:03 -0600

    added IFFE and link JQuery

---
+++ scripts/formhandler.js
@@ -0,0 +1,13 @@
+(function (window) {
+    'use strict';
+
+    let App = window.App || {};
+
+    function FormHandler() {
+        // do stuff here 
+    }
+
+    App.FormHandler = FormHandler;
+    window.App = App;
+
+})(window);
