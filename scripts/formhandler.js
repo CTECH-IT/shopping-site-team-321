0e019223da9d849d4a928c9d746b73a552e96e67
Elizabeth Ladwig <7528001@isd535.org>
2021-11-30 09:39:29 -0600

    making the console print data from form

--- scripts/formhandler.js
+++ scripts/formhandler.js
@@ -2,9 +2,33 @@
     'use strict';
 
     let App = window.App || {};
+    let $ = window.jQuery;
 
-    function FormHandler() {
-        // do stuff here 
+    function FormHandler(selector) {
+        if (!selector) {
+            throw new Error('No selector provided!');
+        }
+
+        // find the "selector" in the DOM using jQuery and assign it to this.formElement
+        this.$formEement = $(selector);
+        if (this.$formElement.length == 0) {
+            throw new Error('Could not find element with selector: ' + selector);
+        } 
+    }
+
+    FormHandler.prototype.addSubmitHandler = function (func) {
+        console.log('Setting the submit handler for the form...');
+        this.$formElement.on('submit', function(event) {
+            event.preventDefault();
+
+            let data = {};
+            $(this).serialArray().forEach(function (item) {
+                data[item.name] = item.value;
+                console.log(item.name + ' is ' + item.value); 
+            });
+            console.log(data);
+            func(data);
+        });
     }
 
     App.FormHandler = FormHandler;
